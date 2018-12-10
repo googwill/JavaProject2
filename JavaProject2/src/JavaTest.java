a701e4b9afb6ecf48d00e8462c351793adf1e666
Z1 <Z1@Z1-HP>
2018-12-09 20:25:14 -0500

    2nd checkin of JavaProject 2

--- JavaProject2/src/JavaTest.java
+++ JavaProject2/src/JavaTest.java
@@ -4,7 +4,9 @@ public class JavaTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is a test");
-
+		System.out.println("1111");
+		
+		//test
 	}
 
 }
